34e900741719ab3c6bb508f374300efbee16b573
obartkov <oleksandr.bartkov@avid.com>
2024-04-02 14:22:19 -0400

    Initialized Spring Boot Project

---
+++ src/main/java/com/aba/archiver/AwsArchiverApplication.java
@@ -0,0 +1,16 @@
+package com.aba.archiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * AWS Archiver Back End Main Application class
+ */
+@SpringBootApplication
+public class AwsArchiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsArchiverApplication.class, args);
+	}
+
+}
